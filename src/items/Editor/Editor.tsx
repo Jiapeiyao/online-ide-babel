67d507427e3bd06e5a4753f761b001cfb766ac08
Jiapeiyao <charlesyjp@qq.com>
2020-05-12 03:43:49 +0800

    set babel standalone as external

--- src/items/Editor/Editor.tsx
+++ src/items/Editor/Editor.tsx
@@ -1,42 +1,45 @@
 import React from 'react';
-import MonacoEditor, { ChangeHandler, EditorDidMount } from 'react-monaco-editor';
+import MonacoEditor, {
+  ChangeHandler,
+  EditorDidMount,
+} from 'react-monaco-editor';
 import { GlobalContext } from '../Root';
 
 interface EditorProps {
-    width: number | string;
-    height: number | string;
+  width: number | string;
+  height: number | string;
 }
 
 export default function Editor({ width, height }: EditorProps) {
-    const [context, dispatch] = React.useContext(GlobalContext);
-    const onChange: ChangeHandler = (newTsx, event) => {
-        dispatch({
-            type: 'tsx',
-            tsx: newTsx,
-        });
-    }
+  const [context, dispatch] = React.useContext(GlobalContext);
+  const onChange: ChangeHandler = (newTsx, event) => {
+    dispatch({
+      type: 'tsx',
+      tsx: newTsx,
+    });
+  };
 
-    const editorDidMount: EditorDidMount = (editor, monaco) => {
-        monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-            jsx: monaco.languages.typescript.JsxEmit.React,
-        });
+  const editorDidMount: EditorDidMount = (editor, monaco) => {
+    monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+      jsx: monaco.languages.typescript.JsxEmit.React,
+    });
 
-        monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
-            noSemanticValidation: true,
-            noSyntaxValidation: true,
-            noSuggestionDiagnostics: true,
-        });
-    }
+    monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+      noSemanticValidation: false,
+      noSyntaxValidation: false,
+      noSuggestionDiagnostics: false,
+    });
+  };
 
-    return  (
-        <MonacoEditor
-            width={width}
-            height={height}
-            value={context.tsx}
-            language='typescript'
-            theme='vs-dark'
-            editorDidMount={editorDidMount}
-            onChange={onChange}
-        />
-    );
+  return (
+    <MonacoEditor
+      width={width}
+      height={height}
+      value={context.tsx}
+      language="typescript"
+      theme="vs-dark"
+      editorDidMount={editorDidMount}
+      onChange={onChange}
+    />
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,101 +1,118 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-const distPath = path.resolve(__dirname, "dist");
+const distPath = path.resolve(__dirname, 'dist');
 
-const postcssLoaderOptions = {
+const postcssLoader = {
+  loader: 'postcss-loader',
+  options:  {
     plugins: [require('autoprefixer')],
     javascriptEnabled: true,
+  },
+};
+
+const tsLoader = {
+  loader: 'ts-loader',
+  options: {
+    happyPackMode: true,
+  },
 };
 
 const config = {
-    entry: ["./src/index.tsx"],
-    output: {
-        path: distPath,
-        filename: "app.js",
-    },
-    devServer: {
-        contentBase: distPath,
-    },
-    devtool: "source-map",
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    "style-loader",
-                    {
-                        loader: "css-loader",
-                        options: {
-                            importLoaders: 1,
-                        },
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: postcssLoaderOptions,
-                    },
-                ],
-                exclude: /\.module\.css$/,
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    "style-loader",
-                    {
-                        loader: "css-loader",
-                        options: {
-                            importLoaders: 1,
-                            modules: true,
-                        },
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: postcssLoaderOptions,
-                    },
-                ],
-                include: /\.module\.css$/,
-            },
-            {
-                test: /\.less$/,
-                use: ["style-loader", "css-loader", "less-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|gif|ttf)$/,
-                use: "url-loader?limit=100000",
-            },
-        ],
-    },
-    externals: [
-        {
-            'react': 'React',
-            'react-dom': 'ReactDOM',
-            'antd': 'antd',
-        },
-        /^(antd|react|react\-dom)(\/[a-z0-9]*)*$/i,
+  entry: { 
+    app: './src/index.tsx',
+  },
+  output: {
+    path: distPath,
+    filename: '[name].js',
+  },
+  devServer: {
+    contentBase: distPath,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)$/,
+        use: ['thread-loader', tsLoader],
+        exclude: /node_modules/,
+      },
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     'style-loader',
+      //     {
+      //       loader: 'css-loader',
+      //       options: {
+      //         importLoaders: 1,
+      //       },
+      //     },
+      //     postcssLoader
+      //   ],
+      //   exclude: /\.module\.css$/,
+      // },
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     'style-loader',
+      //     {
+      //       loader: 'css-loader',
+      //       options: {
+      //         importLoaders: 1,
+      //         modules: true,
+      //       },
+      //     },
+      //     postcssLoader
+      //   ],
+      //   include: /\.module\.css$/,
+      // },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|ttf)$/,
+        use: 'url-loader?limit=100000',
+      },
     ],
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
+  },
+  externals: [
+    {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      antd: 'antd',
+      '@babel/standalone': 'Babel',
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html'),
-            inject: true,
-            appMountId: "app",
-            filename: "index.html",
-        }),
-        new MonacoWebpackPlugin({
-            // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
-            languages: ['typescript', 'javascript', 'less', 'typescriptreact']
-        })
-    ],
+    /^(antd|react|react\-dom)(\/[a-z0-9]*)*$/i,
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      inject: true,
+      appMountId: 'app',
+      filename: 'index.html',
+    }),
+    new MonacoWebpackPlugin({
+      // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+      languages: ['typescript', 'less'],
+      features: [],
+      // features: ['accessibilityHelp', 'bracketMatching', 'caretOperations', 'clipboard', 'codeAction', 'codelens', 'colorDetector', 'comment', 'contextmenu', 'coreCommands', 'cursorUndo', 'dnd', 'find', 'folding', 'fontZoom', 'format', 'gotoError',  'gotoLine', 'gotoSymbol', 'hover', 'iPadShowKeyboard', 'inPlaceReplace', 'inspectTokens', 'linesOperations', 'links', 'multicursor', 'parameterHints', 'quickCommand', 'quickOutline', 'referenceSearch', 'rename', 'smartSelect', 'snippets', 'suggest', 'toggleHighContrast', 'toggleTabFocusMode', 'transpose', 'wordHighlighter', 'wordOperations', 'wordPartOperations']
+    }),
+  ],
 };
 
 module.exports = (env, argv) => {
-    return config;
+  return config;
 };
